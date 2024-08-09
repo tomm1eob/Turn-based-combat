8b3aff8a84ec2f163d7caed732553d31a660bce1
tomm1eob <tomiobredorsteam@gmail.com>
2024-08-08 21:06:54 -0300

    Declaración de clase player y items

---
+++ items.py
@@ -0,0 +1,22 @@
+
+# Objetos
+
+## Pociones
+little_potion_health = {"quantity": 1,
+                        "id": 1,
+                        "type": "consumable",
+                        "effect": "increanse_health",
+                        "power": 15 }
+
+little_potion_sanity = {"quantity": 1,
+                        "id": 2,
+                        "type": "consumable",
+                        "type": "increanse_sanity",
+                        "power": 15 }
+
+little_potion_damage = {"quantity": 1,
+                        "id": 3,
+                        "type": "consumable",
+                        "effect": "increanse_damage",
+                        "power": 10 }
+
---
+++ player.py
@@ -0,0 +1,12 @@
+class Player:
+    # Declaraciones 
+
+    inventario = dict()
+    habilidades = dict()
+
+    # Funciones
+
+    def __init__(self,):
+        self.vida = 100
+        self.cordura = 100
+    
